f54596aa1ae5d70dbca188d9231ce6fe13abe25c
unknown <vladocrat@gmail.com>
2024-01-10 23:22:52 +0300

    fixed translation comparison issues

--- headers/translation.h
+++ headers/translation.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QString>
+#include <tuple>
 
 struct Translation {
     QString furigana;
@@ -12,10 +13,18 @@ struct Translation {
         return "Furigana: " + furigana + " Kanji: " + kanji + " Meaning: " + meaning;
     }
 
-    [[nodiscard]] bool operator==(const Translation& rhs)
+    constexpr auto asTuple() const
     {
-        return this->furigana == rhs.furigana
-               && this->kanji == rhs.kanji
-               && this->meaning == rhs.meaning;
+        return std::tie(furigana, kanji, meaning);
     }
 };
+
+[[nodiscard]] inline bool operator==(const Translation& lhs, const Translation& rhs)
+{
+    return lhs.asTuple() == rhs.asTuple();
+}
+
+[[nodiscard]] inline bool operator!=(const Translation& lhs, const Translation& rhs)
+{
+    return lhs.asTuple() != rhs.asTuple();
+}
